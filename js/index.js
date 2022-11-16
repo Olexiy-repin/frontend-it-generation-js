072d58aa1f1ff8b29dc9135971533ad84783a87a
Olexiy-repin <repinlyoha@gmail.com>
2022-11-16 19:39:33 +0200

    classwork

---
+++ js/index.js
@@ -0,0 +1,30 @@
+/*
+ * Типи даних: number, string, boolean, null та undefined, BigInt, Symbol, Object.
+ */
+
+/*
+ * Виведення даних: console.log та alert. Вкладка Console в Chrome Devtools.
+ */
+
+/*
+ * Змінні: оголошення з let та const.
+ */
+
+// let message;
+
+// console.log(message);
+
+// message = 'Hi!';
+
+// console.log(message);
+
+// const message = 'Hello!';
+
+// console.log(message);
+
+/*
+ * Отримання даних: prompt та confirm.
+ */
+// const userName = confirm('Чи є вам 18?');
+
+// console.log(userName);
---
+++ js/strings.js
@@ -0,0 +1,44 @@
+/*
+ * Конкатенація рядків, шаблонні рядки
+ */
+
+/*
+  ? Запросіть у користувача ім'я з віком та виведіть у консоль повідомлення:
+  ? Привіт ім'я_користувача, ваш вік вік_користувача.
+*/
+// const userFirstName = prompt('Ваше ім`я');
+// const userAge = prompt('Ваш вік');
+
+//? Виконайте завдання вище, використовуючи шаблонні рядки
+
+/*
+ * Властивості і методи рядків
+ */
+
+//? Запитайте у користувача ім'я та дізнайтеся, зі скількох символів, воно складається.
+// const userFirstName = prompt('Ваше ім`я');
+
+//? Попросіть користувача ввести email і переведіть у нижній регістр.
+// const userEmail = prompt('Ваш email');
+
+//? Попросіть користувача ввести email і переведіть у верхній регістр
+// const userEmail = prompt('Ваш email');
+
+/*
+  ? Є рядок 'HTML, CSS, JS, PHP, React, Vue, Node.js, C, C++'.
+  ? Дізнайтеся, чи є в цьому рядку мова: 'JavaScript', або 'JS'?
+*/
+// const languages = 'HTML, CSS, JS, PHP, React, Vue, Node.js, C, C++';
+
+//? У нас є рядок '23,4', замініть кому на крапку
+// const num = prompt('Введіть якесь число');
+
+/*
+  ? У нас є рядок:
+  ? 'The quick brown fox jumps over the lazy dog. If the dog reacted, was it really lazy?'.
+  ? Замініть усі входження dog на monkey.
+*/
+// const str = 'The quick brown fox jumps over the lazy dog. If the dog reacted, was it really lazy?';
+
+//? У нас є дата у форматі '12:05:21', замініть (:) на (.)
+// const someDate = '12:05:21';
